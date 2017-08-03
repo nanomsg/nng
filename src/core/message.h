f16af26ed5fd16ffdfa80924edaf0ca70bb835da
Garrett D'Amore <garrett@damore.org>
2017-08-02 17:15:48 -0700

    Add new PAIR_V1 protocol.
    The PAIR_V1 protocol supports both raw and cooked modes, and has loop
    prevention included.  In raw mode, it allows multiple connections to
    be established (but requires the sender to identify which pipe by ID
    it wishes to use), whereas in cooked mode there is just one.
    
    We default to PAIR_V1 now.
    
    My plan is to make all protocol versioning explicit in the future.

--- src/core/message.h
+++ src/core/message.h
@@ -1,5 +1,6 @@
 //
 // Copyright 2016 Garrett D'Amore <garrett@damore.org>
+// Copyright 2017 Capitar IT Group BV <info@capitar.com>
 //
 // This software is supplied under the terms of the MIT License, a
 // copy of which should be located in the distribution where this
@@ -12,24 +13,32 @@
 
 // Internally used message API.  Again, this is not part of our public API.
 
-extern int    nni_msg_alloc(nni_msg **, size_t);
-extern void   nni_msg_free(nni_msg *);
-extern int    nni_msg_realloc(nni_msg *, size_t);
-extern int    nni_msg_dup(nni_msg **, const nni_msg *);
-extern void * nni_msg_header(nni_msg *);
-extern size_t nni_msg_header_len(nni_msg *);
-extern void * nni_msg_body(nni_msg *);
-extern size_t nni_msg_len(nni_msg *);
-extern int    nni_msg_append(nni_msg *, const void *, size_t);
-extern int    nni_msg_prepend(nni_msg *, const void *, size_t);
-extern int    nni_msg_append_header(nni_msg *, const void *, size_t);
-extern int    nni_msg_prepend_header(nni_msg *, const void *, size_t);
-extern int    nni_msg_trim(nni_msg *, size_t);
-extern int    nni_msg_trunc(nni_msg *, size_t);
-extern int    nni_msg_trim_header(nni_msg *, size_t);
-extern int    nni_msg_trunc_header(nni_msg *, size_t);
-extern int    nni_msg_setopt(nni_msg *, int, const void *, size_t);
-extern int    nni_msg_getopt(nni_msg *, int, void *, size_t *);
-extern void   nni_msg_dump(const char *, const nni_msg *);
+extern int      nni_msg_alloc(nni_msg **, size_t);
+extern void     nni_msg_free(nni_msg *);
+extern int      nni_msg_realloc(nni_msg *, size_t);
+extern int      nni_msg_dup(nni_msg **, const nni_msg *);
+extern void *   nni_msg_header(nni_msg *);
+extern size_t   nni_msg_header_len(nni_msg *);
+extern void *   nni_msg_body(nni_msg *);
+extern size_t   nni_msg_len(nni_msg *);
+extern int      nni_msg_append(nni_msg *, const void *, size_t);
+extern int      nni_msg_prepend(nni_msg *, const void *, size_t);
+extern int      nni_msg_append_header(nni_msg *, const void *, size_t);
+extern int      nni_msg_prepend_header(nni_msg *, const void *, size_t);
+extern int      nni_msg_trim(nni_msg *, size_t);
+extern int      nni_msg_trunc(nni_msg *, size_t);
+extern int      nni_msg_trim_header(nni_msg *, size_t);
+extern int      nni_msg_trunc_header(nni_msg *, size_t);
+extern int      nni_msg_setopt(nni_msg *, int, const void *, size_t);
+extern int      nni_msg_getopt(nni_msg *, int, void *, size_t *);
+extern void     nni_msg_dump(const char *, const nni_msg *);
+extern int      nni_msg_append_u32(nni_msg *, uint32_t);
+extern int      nni_msg_prepend_u32(nni_msg *, uint32_t);
+extern int      nni_msg_header_append_u32(nni_msg *, uint32_t);
+extern int      nni_msg_header_prepend_u32(nni_msg *, uint32_t);
+extern uint32_t nni_msg_trim_u32(nni_msg *);
+extern uint32_t nni_msg_trunc_u32(nni_msg *);
+extern uint32_t nni_msg_header_trim_u32(nni_msg *);
+extern uint32_t nni_msg_header_trunc_u32(nni_msg *);
 
 #endif // CORE_SOCKET_H
