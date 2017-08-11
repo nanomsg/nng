d612eafe174e57bad9061af3ddf0a145fe359591
Garrett D'Amore <garrett@damore.org>
2017-08-11 06:40:44 -0700

    Use updated gcov tool.

--- etc/coverage.sh
+++ etc/coverage.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+# Copyright 2017 Garrett D'Amore <garrett@damore.org>
+# Copyright 2017 Capitar IT Group BV <info@capitar.com>
+#
+# This software is supplied under the terms of the MIT License, a
+# copy of which should be located in the distribution where this
+# file was obtained (LICENSE.txt).  A copy of the license may also be
+# found online at https://opensource.org/licenses/MIT.
+
 if [ "${COVERAGE}" != ON ]
 then
 	echo "Code coverage not enabled."
 	exit 0
 fi
 
+GCOV=${GCOV:-gcov}
+
 # capture all coverage info
-lcov --directory . --capture --output-file coverage.info || exit 1
+lcov --gcov-tool ${GCOV} --directory . --capture --output-file coverage.info || exit 1
 
 # filter out system information (C++ templates & inlines)
 lcov --remove coverage.info '/usr/*' --output-file coverage.info || exit 1
