f8f61093a9818b04941781c0a38c79ca37d350a3
Garrett D'Amore <garrett@damore.org>
2018-05-24 20:43:18 -0700

    Test building the demos.

--- .circleci/build-and-test.sh
+++ .circleci/build-and-test.sh
@@ -4,12 +4,12 @@
 # common build & test steps for CircleCI jobs
 #
 
-uname -a
 cmake --version
 ninja --version
 
 mkdir build
 cd build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_TYPE:-Debug} -DNNG_ENABLE_COVERAGE=${COVERAGE:-OFF} ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_TYPE:-Debug} -DNNG_ENABLE_COVERAGE=${COVERAGE:-OFF} -DBUILD_SHARED_LIBS=${SHARED_LIBS:=ON} ..
 ninja
+ninja install
 env CTEST_OUTPUT_ON_FAILURE=1 ninja test
