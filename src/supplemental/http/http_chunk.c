9ba5cf6b2d070333c62beb195db75f47431ad557
Janjaap Bos <janjaap.bos@capitar.com>
2019-05-17 22:31:10 +0200

    Fix for http chunked transfer

--- src/supplemental/http/http_chunk.c
+++ src/supplemental/http/http_chunk.c
@@ -315,7 +315,7 @@ nni_http_chunks_parse(nni_http_chunks *cl, void *buf, size_t n, size_t *lenp)
 			break;
 
 		case CS_DATA:
-			if ((rv = chunk_ingest_data(cl, src + i, n, &cnt)) !=
+			if ((rv = chunk_ingest_data(cl, src + i, n - i, &cnt)) !=
 			    0) {
 				return (rv);
 			}
