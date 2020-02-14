37c51288e1b9772dfae4440002c128c2ae9d0a66
Ken Haase <kh@beingmeta.com>
2020-02-14 09:29:29 -0500

    Rewrote cmake module macrology and added copyright/release notice

--- cmake/CheckAtomicLib.cmake
+++ cmake/CheckAtomicLib.cmake
@@ -1,12 +1,22 @@
+#
+# Copyright 2020 Kenneth Haase <kh@beingmeta.com>
+#
+# This software is supplied under the terms of the MIT License, a
+# copy of which should be located in the distribution where this
+# file was obtained (LICENSE.txt).  A copy of the license may also be
+# found online at https://opensource.org/licenses/MIT.
+#
+
 # atomic builtins are required for threading support.
 
+INCLUDE(CheckIncludeFiles)
 INCLUDE(CheckCSourceCompiles)
 INCLUDE(CheckLibraryExists)
 
 # Sometimes linking against libatomic is required for atomic ops, if
 # the platform doesn't support lock-free atomics.
 
-function(check_working_c_atomics varname)
+function(check_c_atomics_without_lib varname)
   CHECK_C_SOURCE_COMPILES("
 #include <stdatomic.h>
 int main() {
@@ -19,10 +29,10 @@ int main() {
   return x;
 }
 " ${varname})
-endfunction(check_working_c_atomics)
+endfunction(check_c_atomics_without_lib)
 
-function(check_working_c_atomics_with_lib varname)
-  SET(OLD_CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES})
+function(check_c_atomics_with_lib varname)
+  SET(SAVED_CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES})
   list(APPEND CMAKE_REQUIRED_LIBRARIES "atomic")
   CHECK_C_SOURCE_COMPILES("
 #include <stdatomic.h>
@@ -36,19 +46,19 @@ int main() {
   return x;
 }
 " ${varname})
-  SET(CMAKE_REQUIRED_LIBRARIES "${OLD_CMAKE_REQUIRED_LIBRARIES}")
-endfunction(check_working_c_atomics_with_lib)
+  SET(CMAKE_REQUIRED_LIBRARIES "${SAVED_CMAKE_REQUIRED_LIBRARIES}")
+endfunction(check_c_atomics_with_lib)
 
-macro (CheckAtomic)
+macro (CheckAtomicLib)
   # First check if atomics work without the library.
   if(MSVC)
     set(HAVE_C_ATOMICS_WITHOUT_LIB True)
   else()
-    check_working_c_atomics(HAVE_C_ATOMICS_WITHOUT_LIB)
+    check_c_atomics_without_lib(HAVE_C_ATOMICS_WITHOUT_LIB)
     if(NOT HAVE_C_ATOMICS_WITHOUT_LIB)
-      check_library_exists(atomic __atomic_fetch_add_4 "" HAVE_LIBATOMIC)
+      check_library_exists(atomic __atomic_fetch_add_8 "" HAVE_LIBATOMIC)
       if( HAVE_LIBATOMIC )
-	check_working_c_atomics_with_lib(HAVE_C_ATOMICS_WITH_LIB)
+	check_c_atomics_with_lib(HAVE_C_ATOMICS_WITH_LIB)
 	if (NOT HAVE_C_ATOMICS_WITH_LIB)
 	  message(FATAL_ERROR "Host compiler must support atomic types!")
 	endif()
@@ -57,4 +67,4 @@ macro (CheckAtomic)
       endif()
     endif()
   endif()
-endmacro ()
+endmacro (CheckAtomicLib)
