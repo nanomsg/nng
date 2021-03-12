6cf241a8f80d9323b6ba22c17c54b3eb42ac1d7d
Pierre Pronchery <khorben@defora.org>
2021-03-12 08:28:50 +0100

    Build fix for NetBSD (#1433)

--- src/platform/posix/posix_thread.c
+++ src/platform/posix/posix_thread.c
@@ -262,6 +262,12 @@ nni_plat_thr_set_name(nni_plat_thr *thr, const char *name)
 	if ((thr == NULL) || (pthread_self() == thr->tid)) {
         	pthread_setname_np(name);
 	}
+#elif defined(__NetBSD__)
+	if (thr == NULL) {
+		pthread_setname_np(pthread_self(), "%s", name);
+	} else {
+		pthread_setname_np(thr->tid, "%s", name);
+	}
 #else
 	if (thr == NULL) {
 		pthread_setname_np(pthread_self(), name);
