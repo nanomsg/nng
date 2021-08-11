169221da8d53b2ca4fda76f894bee8505887a7c6
Garrett D'Amore <garrett@damore.org>
2021-08-10 17:09:53 -0700

    Bump version v1.5.2

--- include/nng/nng.h
+++ include/nng/nng.h
@@ -50,7 +50,7 @@ extern "C" {
 // may not necessarily match the ABI versions.
 #define NNG_MAJOR_VERSION 1
 #define NNG_MINOR_VERSION 5
-#define NNG_PATCH_VERSION 1
+#define NNG_PATCH_VERSION 2
 #define NNG_RELEASE_SUFFIX "" // if non-empty, this is a pre-release
 
 // Maximum length of a socket address. This includes the terminating NUL.
