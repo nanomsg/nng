1ae437df62a420cadbd290ed0a4d178e9441353c
Garrett D'Amore <garrett@damore.org>
2021-08-10 17:09:53 -0700

    Bump version v1.5.2

--- include/nng/nng.h
+++ include/nng/nng.h
@@ -49,9 +49,9 @@ extern "C" {
 // We use SemVer, and these versions are about the API, and
 // may not necessarily match the ABI versions.
 #define NNG_MAJOR_VERSION 1
-#define NNG_MINOR_VERSION 4
-#define NNG_PATCH_VERSION 0
-#define NNG_RELEASE_SUFFIX "DEV" // if non-empty, this is a pre-release
+#define NNG_MINOR_VERSION 5
+#define NNG_PATCH_VERSION 2
+#define NNG_RELEASE_SUFFIX "" // if non-empty, this is a pre-release
 
 // Maximum length of a socket address. This includes the terminating NUL.
 // This limit is built into other implementations, so do not change it.
