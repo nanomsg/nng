a6de76c0b1e08ceba5c3e47dd1107f1a09490b0d
Garrett D'Amore <garrett@damore.org>
2023-11-26 12:12:33 -0800

    Bump version to 1.6.0

--- include/nng/nng.h
+++ include/nng/nng.h
@@ -1,5 +1,5 @@
 //
-// Copyright 2021 Staysail Systems, Inc. <info@staysail.tech>
+// Copyright 2023 Staysail Systems, Inc. <info@staysail.tech>
 // Copyright 2018 Capitar IT Group BV <info@capitar.com>
 //
 // This software is supplied under the terms of the MIT License, a
@@ -59,7 +59,7 @@ extern "C" {
 #define NNG_MAJOR_VERSION 1
 #define NNG_MINOR_VERSION 6
 #define NNG_PATCH_VERSION 0
-#define NNG_RELEASE_SUFFIX "pre" // if non-empty, this is a pre-release
+#define NNG_RELEASE_SUFFIX "" // if non-empty (i.e. "pre"), this is a pre-release
 
 // Maximum length of a socket address. This includes the terminating NUL.
 // This limit is built into other implementations, so do not change it.
