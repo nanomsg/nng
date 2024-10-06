b1dd480dc2603a8ede2a32154a959058d9177fb1
Garrett D'Amore <garrett@damore.org>
2024-10-05 17:18:11 -0700

    Fix includes

--- src/core/taskq.h
+++ src/core/taskq.h
@@ -1,5 +1,5 @@
 //
-// Copyright 2022 Staysail Systems, Inc. <info@staysail.tech>
+// Copyright 2024 Staysail Systems, Inc. <info@staysail.tech>
 // Copyright 2018 Capitar IT Group BV <info@capitar.com>
 //
 // This software is supplied under the terms of the MIT License, a
@@ -13,6 +13,7 @@
 
 #include "defs.h"
 #include "list.h"
+#include "platform.h"
 
 typedef struct nni_taskq nni_taskq;
 typedef struct nni_task  nni_task;
