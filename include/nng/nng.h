4cf963ee46c91c666d43cf5f3a9783e32c8906d2
Garrett D'Amore <garrett@damore.org>
2024-11-07 23:45:07 -0800

    Fix version number for 1.9.1 pre.
    We forgot to do this for 1.9.0, which incorrectly claims to be 1.8.0.

--- include/nng/nng.h
+++ include/nng/nng.h
@@ -59,8 +59,8 @@ extern "C" {
 #define NNG_MAJOR_VERSION 1
 #define NNG_MINOR_VERSION 9
 #define NNG_PATCH_VERSION 1
-#define NNG_RELEASE_SUFFIX \
-	"pre" // if non-empty (i.e. "pre"), this is a pre-release
+// if non-empty (i.e. "pre"), this is a pre-release
+#define NNG_RELEASE_SUFFIX "pre"
 
 // Maximum length of a socket address. This includes the terminating NUL.
 // This limit is built into other implementations, so do not change it.
