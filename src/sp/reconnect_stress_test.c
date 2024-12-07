61869fa985ec3454ea23c394c265b6863749408a
Aleksei Solovev <96003044+aleksejsolovev@users.noreply.github.com>
2024-12-07 19:19:26 +0300

    Add forgotten license boilerplate

--- src/sp/reconnect_stress_test.c
+++ src/sp/reconnect_stress_test.c
@@ -1,3 +1,12 @@
+//
+// Copyright 2024 Aleksei Solovev <solovalex@gmail.com>
+//
+// This software is supplied under the terms of the MIT License, a
+// copy of which should be located in the distribution where this
+// file was obtained (LICENSE.txt).  A copy of the license may also be
+// found online at https://opensource.org/licenses/MIT.
+//
+
 #include <assert.h>
 #include <nng/nng.h>
 #include <core/nng_impl.h>
