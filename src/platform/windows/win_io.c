c366471a28e06426fb7d1f6891d424432e450b67
Garrett D'Amore <garrett@damore.org>
2025-02-18 00:08:38 -0800

    Windows: Bring back hEvent -- needed for real IPC.

--- src/platform/windows/win_io.c
+++ src/platform/windows/win_io.c
@@ -64,9 +64,10 @@ nni_win_io_init(nni_win_io *io, nni_win_io_cb cb, void *ptr)
 {
 	ZeroMemory(&io->olpd, sizeof(io->olpd));
 
-	io->cb  = cb;
-	io->ptr = ptr;
-	io->aio = NULL;
+	io->cb          = cb;
+	io->ptr         = ptr;
+	io->aio         = NULL;
+	io->olpd.hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
 }
 
 int
