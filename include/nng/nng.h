f5944ac76adb0b0702570f8c5ff82d5a7ed1924e
schloing <user.datagram.protocol@proton.me>
2026-06-12 21:41:03 +1000

    cleanup: remove nng_alloc_set

--- include/nng/nng.h
+++ include/nng/nng.h
@@ -551,9 +551,6 @@ NNG_DECL int nng_ctx_set_int(nng_ctx, const char *, int);
 NNG_DECL int nng_ctx_set_size(nng_ctx, const char *, size_t);
 NNG_DECL int nng_ctx_set_ms(nng_ctx, const char *, nng_duration);
 
-NNG_DECL int nng_alloc_set(void *(*malloc_fn)(size_t),
-    void *(*calloc_fn)(size_t, size_t), void (*free_fn)(void *));
-
 // nng_alloc is used to allocate memory.  It's intended purpose is for
 // allocating memory suitable for message buffers with nng_send().
 // Applications that need memory for other purposes should use their platform
