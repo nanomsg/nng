d24d91470da2db3103b251cfd7fd60b57f03c5fa
schloing <user.datagram.protocol@proton.me>
2026-06-12 23:28:28 +1000

    fix: declaration for nni_alloc_set

--- src/core/alloc.h
+++ src/core/alloc.h
@@ -4,6 +4,6 @@
 #include <stddef.h>
 
 int nni_alloc_set(void *(*malloc_fn)(size_t),
-    void *(*calloc_fn)(size_t, size_t), void (*free_fn)(void *));
+    void *(*calloc_fn)(size_t, size_t), void (*free_fn)(void *, size_t));
 
 #endif
