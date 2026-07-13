003ef4a2746481e1771984e6c3fdb498856f5567
gdamore <gdamore@users.noreply.github.com>
2026-07-13 00:29:32 +0000

    deploy: 74468330b5298bd8458d3e4505bc5445b1b370f3

--- ref/searcher-c2a407aa.js
+++ ref/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-aff792b3.js',
+                path_to_root + 'searchindex-57b98700.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
